64141779e4a3985c51a9bf352722d061315b9c15
hbwalla <135855643+hbwalla@users.noreply.github.com>
2024-01-12 09:54:30 -0600

    Shape and color buttons are working!
    Updated the files so that when a color or shape icon is clicked, an alert shows up saying that each (respective) button has been pressed. These buttons have been added to the menu bar on the left side. Next big step will be adding an editor component on the screen when one of the buttons is clicked, and being able to change the color of them.

--- my_frontend/app.js
+++ my_frontend/app.js
@@ -24,3 +24,17 @@ function onDrop(event) {
         .dataTransfer
         .clearData();
 }
+
+function onColorButtonClick() {
+    alert("Color button clicked!");
+}
+
+const redButton = document.getElementById("red");
+redButton.addEventListener("click", onColorButtonClick);
+
+function onShapeButtonClick() {
+    alert("Shape button clicked!");
+}
+
+const squareButton = document.getElementById("square");
+squareButton.addEventListener("click", onShapeButtonClick);
