6e139719dca36eba387add559ecf069bc39b49f8
hbwalla <135855643+hbwalla@users.noreply.github.com>
2024-01-24 15:00:53 -0500

    Just deleted a file

--- my_frontend/wStart.js
+++
@@ -1,8 +0,0 @@
-const user = {
-    id: 1,
-    username: "kenny",
-    email: "kenny@southpark.com",
-    password: "shiiiiit",
-    profilePictureUrl: "https://whokilledkenny.com/profilepic.jpeg"
-}
-
