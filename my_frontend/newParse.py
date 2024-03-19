aa6639bbb1c0f39b9439226db8e5737b8565e116
hbwalla <135855643+hbwalla@users.noreply.github.com>
2024-03-18 20:26:12 -0400

    Fixing newParse.py
    Had to convert to list with dictionaries, but it's going well so far. Have to make methods with in a class key-able though somehow.

--- my_frontend/newParse.py
+++ my_frontend/newParse.py
@@ -1,142 +1,33 @@
 import ast
 import json
 
-# class ClassFunctionVisitor(ast.NodeVisitor):
-#     def __init__(self):
-#         self.output_file = open("/Users/haileywallace/Desktop/orb_dev/Orb24/my_frontend/text.txt", "w")
-#         self.in_class = False
-#         self.function_nesting_level = 0
 
-#     def visit_ClassDef(self, node):
-#         self.in_class = True
-#         print(f'Class: {node.name}')
-#         self.output_file.write(f'Class: {node.name} \n')
-#         # self.output_file.write('---\n')
-#         self.generic_visit(node)
-#         self.in_class = False
-#         print('---')  # Separator between classes
-
-#     def visit_FunctionDef(self, node):
-#         if self.in_class:
-#             prefix = '  Method' if self.function_nesting_level == 0 else '    Nested Function'
-#             args = [arg.arg for arg in node.args.args]
-#             print(f'  Nested Function: {node.name}')
-#             self.output_file.write(f'   Nested Function: {node.name} \n')
-#             self.output_file.write(f'       Arguments: {args} \n')
-#         else:
-#             prefix = 'Function' if self.function_nesting_level == 0 else '  Nested Function'
-#             args = [arg.arg for arg in node.args.args]
-#             print(f'Function: {node.name}')
-#             self.output_file.write(f'Function: {node.name} \n')
-#             self.output_file.write(f'       Arguments: {args} \n')
-
-#         print(f'{prefix} (Level {self.function_nesting_level}): {node.name}')
-#         # self.output_file.write(f'{prefix} (Level {self.function_nesting_level}): {node.name}')
-
-#         # args = [arg.arg for arg in node.args.args]
-#         # print(f'  Arguments: {args}')
-#         # self.output_file.write(f'   Arguments: {args} \n')
-
-#         self.function_nesting_level += 1
-#         self.generic_visit(node)
-#         self.function_nesting_level -= 1
-
-
-# # need to update to account for nested functions
-
-# def extract_classes_functions(filename):
-#     with open(filename, 'r') as file:
-#         tree = ast.parse(file.read(), filename=filename)
-#     visitor = ClassFunctionVisitor()
-#     visitor.visit(tree)
-
-# if __name__ == '__main__':
-#     extract_classes_functions('/Users/haileywallace/Desktop/Brain Child/linear_algebra.py')
-
-# -------------------
-
-
-# class ClassFunctionVisitor(ast.NodeVisitor):
-#     def __init__(self):
-#         self.dictionary1 = dict()
-#         self.output_file = open("/Users/haileywallace/Desktop/orb_dev/Orb24/my_frontend/text.txt", "w")
-#         self.in_class = False
-#         self.function_nesting_level = 0
-
-#     def visit_ClassDef(self, node):
-#         self.in_class = True
-#         self.dictionary2 = dict()
-#         print(f'Class: {node.name}')
-#         self.dictionary2["class:"] = node.name
-#         self.generic_visit(node)
-#         self.in_class = False
-
-#     def visit_FunctionDef(self, node):
-#         if self.in_class:
-#             prefix = '  Method' if self.function_nesting_level == 0 else '    Nested Function'
-#             args = [arg.arg for arg in node.args.args]
-#             print(f'  Nested Function: {node.name}')
-#             self.output_file.write(f'   Nested Function: {node.name} \n')
-#             self.dictionary["nested function:"] = {node.name}
-#             self.output_file.write(f'       Arguments: {args} \n')
-#             self.dictionary["arguments:"] = {i for i in args}
-#         else:
-#             prefix = 'Function' if self.function_nesting_level == 0 else '  Nested Function'
-#             args = [arg.arg for arg in node.args.args]
-#             print(f'Function: {node.name}')
-#             self.dictionary3 = dict()
-#             self.output_file.write(f'Function: {node.name} \n')
-#             self.dictionary3["function:"] = {node.name}
-#             self.output_file.write(f'       Arguments: {args} \n')
-#             self.dictionary3["arguments"] = {i for i in args}
-
-#         print(f'{prefix} (Level {self.function_nesting_level}): {node.name}')
-#         # self.output_file.write(f'{prefix} (Level {self.function_nesting_level}): {node.name}')
-
-#         # args = [arg.arg for arg in node.args.args]
-#         # print(f'  Arguments: {args}')
-#         # self.output_file.write(f'   Arguments: {args} \n')
-
-#         self.function_nesting_level += 1
-#         self.generic_visit(node)
-#         self.function_nesting_level -= 1
-#         newdict = self.dictionary1 | self.dictionary2 | self.dictionary3
-#         print(newdict)
-
-
-# # need to update to account for nested functions
-
-# def extract_classes_functions(filename):
-#     with open(filename, 'r') as file:
-#         tree = ast.parse(file.read(), filename=filename)
-#     visitor = ClassFunctionVisitor()
-#     visitor.visit(tree)
-
-# if __name__ == '__main__':
-#     extract_classes_functions('/Users/haileywallace/Desktop/Brain Child/linear_algebra.py')
-
-
-class ClassFunctionVisitor(ast.NodeVisitor):
+class PyFileVisitor(ast.NodeVisitor):
     def __init__(self):
         self.result_dict = {}
+        self.result_list = []
         self.current_class = None
         self.function_nesting_level = 0
 
     def visit_ClassDef(self, node):
-        self.current_class = node.name
-        self.result_dict[node.name] = {"type": "class", 'methods': {}} # update method?
-        # classdict = {"type": "class", "name": node.name, "methods": {}}
-        # self.result_dict.update(classdict)
+        # self.current_class = node.name
+        class_info = {"type": "class", "name": node.name, "methods": []}
+        # self.result_dict[node.name] = {"type": "class", 'methods': {}} # update method?
+        self.result_list.append(class_info)
+        self.current_class = class_info
         self.generic_visit(node)
         self.current_class = None
 
     def visit_FunctionDef(self, node):
         args = [arg.arg for arg in node.args.args]
-        function_info = {"type": "function", 'arguments': args, 'level': self.function_nesting_level}
+        # function_info = {"type": "function", 'arguments': args, 'level': self.function_nesting_level}
+        function_info = {"type": "function", "name": node.name, 'arguments': args, 'level': self.function_nesting_level}
         if self.current_class:
-            self.result_dict[self.current_class]['methods'][node.name] = function_info
+            # self.result_dict[self.current_class]['methods'][node.name] = function_info
+            self.current_class["methods"].append(function_info)
         else:
-            self.result_dict[node.name] = function_info
+            # self.result_dict[node.name] = function_info
+            self.result_list.append(function_info)
 
         self.function_nesting_level += 1
         self.generic_visit(node)
@@ -145,14 +36,40 @@ class ClassFunctionVisitor(ast.NodeVisitor):
 def extract_classes_functions(filename):
     with open(filename, 'r') as file:
         tree = ast.parse(file.read(), filename=filename)
-    visitor = ClassFunctionVisitor()
+    visitor = PyFileVisitor()
     visitor.visit(tree)
-    # with open("result.json", "w") as fp:
-    #     json.dump(visitor.result_dict, fp)
-    return visitor.result_dict
+    # return visitor.result_dict
+    return visitor.result_list
 
 if __name__ == '__main__':
     result = extract_classes_functions('/Users/haileywallace/Desktop/Brain Child/linear_algebra.py')
     with open('/Users/haileywallace/Desktop/orb_dev/Orb24/my_frontend/output.json', "w") as outfile:
         json.dump(result, outfile, indent = 4)
     print(result)
+
+# have input for arguments in a function be a tuple to assign types (or multiple possible types)
+# switch indentation of type and name of the function/class
+# types have to match type
+# self is a different data type, don't assign or delete it
+# fix nesting level to update (inside function/class = level 1)
+# functions within a class are called methods
+# have this recognize files too
+# lightweight ID with each block, could be generated here; ID for file, ID for code block in file, only checking 
+    # once for file ID
+# certain fileIDs/blocks will be written as connected to each other, positioned in canvas, save representation
+    # of blocks. saves positioning
+
+# drawing app- canva- for references for pan/zoom
+# look at node-red
+# administrators will populate code base with new code, including JSON associated with file
+# save current state or several previous states- undo doesn't have a ton of utility
+# compare changes in version management- if no changes made, don't update to server
+# make new JSON file with changes
+# Want to be able to name canvas and save, if account associated with canvases can open
+    # and save
+# How to organize toolbar?
+    # like it node-red, file name dropdown, classes, etc, search bar, this will happen 
+    # after I can put the file into the UI
+# add connector not really necessary
+    
+# update: make "methods" within a class readable
--- my_frontend/organize.js
+++ my_frontend/organize.js
@@ -1,8 +1,23 @@
 const sample = require("./output.json");
 
-firstArg = JSON.stringify(sample.vector.methods.__init__.arguments[0]);
+function range(start, end) {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx)
+};
 
+var size = Object.keys(sample).length;
 
+let index = 0;
+let result = range(0, size - 1);
 
+while (index < result.length) {
+    var methods = JSON.stringify(sample[result[index]]["methods"]);
+    console.log(methods);
+    
+    index++;
+}
 
+// console.log(result)
 
+// firstArg = JSON.stringify(sample[range]);
+
+// console.log(firstArg);
